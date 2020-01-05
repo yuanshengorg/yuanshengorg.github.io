8c1cc0af43717232df2230a2d532b53006b33ba7
yuansheng <740040915@qq.com>
2020-01-06 02:41:54 +0800

    Site updated: 2020-01-06 02:41:38

--- css/fonts/new/icomoon/demo-files/demo.js
+++ css/fonts/new/icomoon/demo-files/demo.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 02 2020 23:52:03 GMT+0800 (GMT+08:00)
+// build time:Mon Jan 06 2020 02:41:34 GMT+0800 (GMT+08:00)
 if(!("boxShadow"in document.body.style)){document.body.setAttribute("class","noBoxShadow")}document.body.addEventListener("click",function(e){var t=e.target;if(t.tagName==="INPUT"&&t.getAttribute("class").indexOf("liga")===-1){t.select()}});(function(){var e=document.getElementById("fontSize"),t=document.getElementById("testDrive"),n=document.getElementById("testText");function o(){t.innerHTML=n.value||String.fromCharCode(160);if(window.icomoonLiga){window.icomoonLiga(t)}}function i(){t.style.fontSize=e.value+"px"}e.addEventListener("change",i,false);n.addEventListener("input",o,false);n.addEventListener("change",o,false);i()})();
 //rebuild by neat 
